81a4f6ab73d6ce5f63d1b8265ef3a01ad2ea5d99
csheng6 <shengafur@gmail.com>
2024-02-15 02:55:27 -0500

    Created function to randomly create entrance and exit, break walls in the grid using DFS recursively using adjacency list and solving the maze with DFS recursively.

--- maze.py
+++ maze.py
@@ -11,7 +11,8 @@ class Maze:
             num_cols,
             cell_size_x,
             cell_size_y,
-            win=None
+            win=None,
+            seed=None
             ):
         self._x1 = x1
         self._y1 = y1
@@ -21,7 +22,15 @@ class Maze:
         self.cell_size_y = cell_size_y
         self._win = win
         self._cells = []
+        self._adj_list = {}
+        if seed:
+            random.seed(seed)
         self._create_cells()
+        self._build_adj_list()
+        self._break_entrance_and_exit()
+        self._break_walls_r(0,0)
+        self._reset_cells_visited(0,0)
+        self.solve()
 
     def _create_cells(self):
         for row in range(self.num_rows):
@@ -32,6 +41,7 @@ class Maze:
         for col in range(self.num_cols):
             for row in range(self.num_rows):
                 self._draw_cell(row, col)
+        self._build_adj_list()
 
     def _draw_cell(self, row, col):
         if self._win is None:
@@ -64,3 +74,106 @@ class Maze:
             self._cells[row][col].has_bottom_wall = False
         self._draw_cell(0,0)
         self._draw_cell(row,col)
+
+    def _break_walls_r(self, row_coord, col_coord):
+        self._cells[row_coord][col_coord].visited = True
+        copy_adj = self._adj_list[self._cells[row_coord][col_coord]]
+        random.shuffle(copy_adj)
+        for adjace in copy_adj:
+            if not adjace['cell'].visited:
+                self._break_walls_r(adjace['row'], adjace['col'])
+                if adjace['neighbor'] == 'right':
+                    self._cells[row_coord][col_coord].has_right_wall = False
+                elif adjace['neighbor'] == 'bottom':
+                    self._cells[row_coord][col_coord].has_bottom_wall = False
+                elif adjace['neighbor'] == 'top':
+                    self._cells[row_coord][col_coord].has_top_wall = False
+                elif adjace['neighbor'] == 'left':
+                    self._cells[row_coord][col_coord].has_left_wall = False
+                self._draw_cell(row_coord, col_coord)
+
+    def _build_adj_list(self):
+        for col in range(self.num_cols):
+            for row in range(self.num_rows):
+                edges = []
+                check_bottom_neighbor, check_left_neighbor, check_right_neighbor, check_top_neighbor = True, True, True, True
+                for num_poss_edges in range(4):
+                    try:
+                        if check_bottom_neighbor:
+                            edges.append({'cell': self._cells[row][col+1], 'row': row, 'col': col+1, 'neighbor': 'bottom'})
+                            check_bottom_neighbor = False
+                    except:
+                        check_bottom_neighbor = False
+                    try:
+                        if check_right_neighbor:
+                            edges.append({'cell': self._cells[row+1][col], 'row': row+1, 'col': col, 'neighbor':  'right'})
+                            check_right_neighbor = False
+                    except:
+                     check_right_neighbor = False
+                    try:
+                        if check_top_neighbor and col-1 >= 0:
+                            edges.append({'cell': self._cells[row][col-1], 'row': row, 'col': col-1, 'neighbor':  'top'})
+                            check_top_neighbor = False
+                        else:
+                            check_top_neighbor = False
+                    except:
+                        check_top_neighbor = False
+                    try:
+                        if check_left_neighbor and row-1 >= 0:
+                            edges.append({'cell': self._cells[row-1][col], 'row': row-1, 'col': col, 'neighbor':  'left'})
+                            check_left_neighbor = False
+                        else:
+                            check_left_neighbor = False
+                    except:
+                        check_left_neighbor = False
+
+                self._adj_list[self._cells[row][col]] = edges
+
+    def _reset_cells_visited(self, row_coord, col_coord):
+        if not self._cells[row_coord][col_coord].visited:
+            return
+        else:
+            self._cells[row_coord][col_coord].visited = False
+        for adjace in self._adj_list[self._cells[row_coord][col_coord]]:
+            self._reset_cells_visited(adjace['row'], adjace['col'])
+
+    def solve(self):
+        return self._solve_r(0,0)
+    
+    def _solve_r(self, row_coord, col_coord):
+        if self._cells[row_coord][col_coord].visited:
+            return
+        if row_coord == self.num_rows-1 and col_coord == self.num_cols-1:
+            return True
+        self._animate()
+        self._cells[row_coord][col_coord].visited = True
+        for adjace in self._adj_list[self._cells[row_coord][col_coord]]:
+            if adjace['neighbor'] == 'right':
+                if not self._cells[row_coord][col_coord].has_right_wall:
+                    self._cells[row_coord][col_coord].draw_move(adjace['cell'])
+                    if self._solve_r(adjace['row'], adjace['col']):
+                        return True
+                    else:
+                        self._cells[row_coord][col_coord].draw_move(adjace['cell'], undo=True)
+            elif adjace['neighbor'] == 'bottom':
+                if not self._cells[row_coord][col_coord].has_bottom_wall:
+                    self._cells[row_coord][col_coord].draw_move(adjace['cell'])
+                    if self._solve_r(adjace['row'], adjace['col']):
+                        return True
+                    else:
+                        self._cells[row_coord][col_coord].draw_move(adjace['cell'], undo=True)
+            elif adjace['neighbor'] == 'left':
+                if not self._cells[row_coord][col_coord].has_left_wall:
+                    self._cells[row_coord][col_coord].draw_move(adjace['cell'])
+                    if self._solve_r(adjace['row'], adjace['col']):
+                        return True
+                    else:
+                        self._cells[row_coord][col_coord].draw_move(adjace['cell'], undo=True)
+            elif adjace['neighbor'] == 'top':
+                if not self._cells[row_coord][col_coord].has_top_wall:
+                    self._cells[row_coord][col_coord].draw_move(adjace['cell'])
+                    if self._solve_r(adjace['row'], adjace['col']):
+                        return True
+                    else:
+                        self._cells[row_coord][col_coord].draw_move(adjace['cell'], undo=True)
+        return False
--- tests.py
+++ tests.py
@@ -30,6 +30,15 @@ class Tests(unittest.TestCase):
         m1._break_entrance_and_exit()
         self.assertIsNot(m1._cells[0][0].has_top_wall, m1._cells[0][0].has_left_wall)
         self.assertIsNot(m1._cells[(m1.num_rows)-1][(m1.num_cols)-1].has_right_wall, m1._cells[(m1.num_rows)-1][(m1.num_cols)-1].has_bottom_wall)
+    
+    def test_maze_break_walls_r(self):
+        num_cols = 10
+        num_rows = 10
+        cell_size_x = 10
+        cell_size_y = 10
+        m1 = Maze(0, 0, num_rows, num_cols, cell_size_x, cell_size_y)
+        m1._break_walls_r(0,0)
+        self.assertIsNot(m1._cells[0][0].has_bottom_wall, m1._cells[0][0].has_right_wall)
 
 if __name__ == "__main__":
     unittest.main()
